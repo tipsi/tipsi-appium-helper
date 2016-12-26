a8410a161a61f49a0ed8a0e11668f0d385b48dfa
Anton Petrov <petrov.aap@gmail.com>
2016-12-26 23:11:17 +0700

    Resolve issues with CLI command

--- bin/appium-helper.js
+++ bin/appium-helper.js
@@ -1,5 +1,13 @@
-/* eslint no-var: 0 */
-var config = require('../src/core/configuration')
-var run = require('../src/run')
+#!/usr/bin/env node
+
+require('babel-polyfill')
+require('babel-register')({
+  ignore: false,
+  only: /tipsi_appium\/src/,
+})
+
+/* eslint no-var: 0 vars-on-top: 0 */
+var config = require('../src/core/configuration').default
+var run = require('../src/run').default
 
 run(config)
