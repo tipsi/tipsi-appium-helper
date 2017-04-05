de7d1421ee147e354b168a05979e538695a5162d
Anton Kuznetsov <isnifer@gmail.com>
2017-04-05 14:41:39 +0300

    Replace pastebin with github gist (#8)

--- src/plugins/source.js
+++ src/plugins/source.js
@@ -1,28 +1,27 @@
-import pastebin from 'better-pastebin'
+import quickGist from 'quick-gist'
 
 export default async function () {
-  pastebin.setDevKey(this.config.pastebin)
-
-  const source = await this.driver.getSource()
+  const content = await this.driver.getSource()
 
   return new Promise((resolve) => {
-    pastebin.create({
-      contents: source,
-      privacy: 1,
-      format: 'xml',
-      name: `Appium source ${new Date()}`,
-    }, (success, data) => {
+    quickGist({
+      content,
+      description: `Appium source ${new Date()}`,
+      public: false,
+      enterpriseOnly: false,
+      fileExtension: 'xml',
+    }, (error, resp, data) => {
       /* eslint-disable no-console */
-      if (success) {
+      if (error) {
         console.log('---------------------------------------------------')
-        console.log('SOURCE URL:', data)
-        console.log('---------------------------------------------------')
-        resolve(data)
-      } else {
-        console.log('---------------------------------------------------')
-        console.log('Failed to save source:', data.message)
+        console.log('Failed to save source:', error.message)
         console.log('---------------------------------------------------')
         resolve(null)
+      } else {
+        console.log('---------------------------------------------------')
+        console.log('SOURCE URL:', data.html_url)
+        console.log('---------------------------------------------------')
+        resolve(data)
       }
       /* eslint-enable no-console */
     })
