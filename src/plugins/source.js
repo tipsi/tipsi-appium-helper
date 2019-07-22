e4abaab5cd378e8c416cb3dbdc66ca8167e28481
Dave MacDonald <mindlapse@gmail.com>
2019-07-22 16:56:12 -0400

    Switched to ISO 8601 timestamp for source logs

--- src/plugins/source.js
+++ src/plugins/source.js
@@ -1,12 +1,12 @@
 import path from 'path'
 import fs from 'fs'
-import _ from 'lodash'
+import date from 'date-and-time'
 
 export default async function () {
   const content = await this.driver.getSource()
 
   return new Promise((resolve) => {
-    const pathToLog = path.resolve(process.cwd(), _.uniqueId('appium_source_')+'.xml')
+    const pathToLog = path.resolve(process.cwd(), date.format(new Date(), 'YYYYMMDD[T]HHmmss')+'.xml')
     fs.writeFile(pathToLog, content, 'utf8', (error) => {
       if (error) {
         console.log('---------------------------------------------------')
@@ -20,5 +20,6 @@ export default async function () {
         resolve()
       }
     })
+    return pathToLog
   })
 }
